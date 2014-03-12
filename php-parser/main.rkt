153c3325b4bdd09e01777df6f09060ed29c63303
Antoine Brand <antoine.brand@sfr.fr>
2014-03-12 21:34:47 +0100

    * Provide Position struct

--- php-parser/main.rkt
+++ php-parser/main.rkt
@@ -1,23 +1,14 @@
 #lang racket
 
 (require "parser.rkt"
-	 (only-in "parser-utils.rkt"
-		   sub-ast?
-		   get-sub-ast
-		   ast-struct
-		   position-start-offset
-		   position-end-offset
-		   prop:sub-ast
-		   extra-ast?
-		   get-extra-ast
-		   set-extra-ast!
-		   Extra?
-		   Extra
-		   Extra-parent
-		   Extra-type))
+	 "parser-utils.rkt")
 
 (provide
  (all-from-out "parser.rkt")
+ Position
+ Position-start
+ Position-end
+ Position?
  sub-ast?
  get-sub-ast
  ast-struct
